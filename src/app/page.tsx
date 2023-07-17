4cacbaf7b07bec44c4301ccbf38b89f8fd266eb2
smith-doubleme-me <smith.jang@doubleme.me>
2023-07-17 12:09:38 +0900

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,16 @@ import { Canvas } from "@react-three/fiber"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <h1 className="text-5xl font-bold text-blue-100">Fiber test</h1>
+      <div className="w-screen h-screen">
+        <Canvas>
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+      </div>
     </main>
   )
 }
