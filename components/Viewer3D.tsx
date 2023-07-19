8917943fb1171b079c4b38d561d21a69a905e3c9
smith-doubleme-me <smith.jang@doubleme.me>
2023-07-19 12:16:55 +0900

    3d model test

--- components/Viewer3D.tsx
+++ components/Viewer3D.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { useGLTF, Bounds } from "@react-three/drei";
+import { useGLTF, Bounds, PerspectiveCamera } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const Viewer3D = () => {
   const modelRef = useRef<any>(null);
-  const path = "/models/test3.glb";
+  const cameraRef = useRef<any>(null);
+
+  const path = "/models/test2.glb";
 
   const result = useGLTF(path);
 
   const scaleModelToFit = () => {
     const box = new THREE.Box3().setFromObject(modelRef.current);
     const modelSize = new THREE.Vector3();
+
     box.getSize(modelSize);
 
     // 원하는 크기로 스케일링
@@ -20,7 +23,6 @@ const Viewer3D = () => {
     const scaleFactor =
       desiredSize / Math.max(modelSize.x, modelSize.y, modelSize.z);
 
-    console.log(scaleFactor);
     const getScaleSize = () => {
       if (scaleFactor > 50) {
         return scaleFactor * 0.02;
@@ -46,9 +48,19 @@ const Viewer3D = () => {
   }, []);
 
   return (
-    // <Bounds fit clip observe damping={6} margin={1.2}>
-    <primitive object={result.scene} ref={modelRef} />
-    // </Bounds>
+    <PerspectiveCamera
+      ref={cameraRef}
+      fov={60}
+      aspect={1200 / 600}
+      near={0.1}
+      far={1000}
+    >
+      {/* <Bounds fit clip observe damping={6} margin={1.2}> */}
+      <group position={[0, 0, 0]}>
+        <primitive object={result.scene} ref={modelRef} />
+      </group>
+      {/* </Bounds> */}
+    </PerspectiveCamera>
   );
 };
 
