23dd3dd05ad8d0f3b102030c0cafcd8bc93a0e74
smith-doubleme-me <smith.jang@doubleme.me>
2023-07-25 13:53:44 +0900

    rotate 360 snapshot

--- components/ModelCanvas.tsx
+++ components/ModelCanvas.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useCallback, useRef, useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
+import { useCallback, useRef, useState } from "react";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { Environment, OrbitControls, Stage, useGLTF } from "@react-three/drei";
 import { Suspense } from "react";
 
@@ -13,74 +13,53 @@ const ModelCanvas = () => {
   const cameraRef = useRef<any>();
   const currentAngleRef = useRef<number>(0);
 
-  const path = "/models/deer.glb";
+  // const path = "/models/deer.glb";
 
-  const result = useGLTF(path);
-  result.scene.updateMatrixWorld();
+  // const result = useGLTF(path);
+  // result.scene.updateMatrixWorld();
 
-  const Viewer = () => {
-    const { camera } = useThree();
+  const [capturing, setCapturing] = useState<boolean>(false);
 
-    cameraRef.current = camera;
-
-    return <primitive object={result.scene} />;
+  const captureImage = () => {
+    if (!capturing) {
+      setCapturing(true);
+    }
   };
 
-  const handleSnapshot = useCallback(() => {
-    const angles = [];
+  const capture = () => {
+    const strMime = "image/jpg";
+    const strDownloadMime = "image/octet-stream";
 
-    for (let i = 0; i < 12; i++) {
-      angles.push((i * 30 * Math.PI) / 180);
-    }
-
-    if (currentAngleRef.current >= angles.length) return;
-
-    const angle = angles[currentAngleRef.current];
-    controlsRef.current.target.set(0, 0, 0);
-    controlsRef.current.enabled = false;
-
-    cameraRef.current.position.x = Math.cos(angle) * 10;
-    cameraRef.current.position.z = Math.sin(angle) * 10;
-    cameraRef.current.lookAt(0, 0, 0);
-
-    setTimeout(() => {
-      currentAngleRef.current += 1;
-      controlsRef.current.enabled = true;
-
-      const strMime = "image/jpg";
-      const strDownloadMime = "image/octet-stream";
-
-      const imgData = canvasRef.current
-        .toDataURL(strMime)
-        .replace(strMime, strDownloadMime);
-      const link = document.createElement("a");
-      document.body.appendChild(link);
-      link.download = "test.jpg";
-      link.href = imgData;
-      link.click();
-      document.body.removeChild(link);
-      handleSnapshot();
-    }, 100);
-  }, [canvasRef.current, controlsRef.current]);
+    const imgData = canvasRef.current
+      .toDataURL(strMime)
+      .replace(strMime, strDownloadMime);
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    link.download = "test.jpg";
+    link.href = imgData;
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div>
       <button
-        onClick={handleSnapshot}
+        onClick={captureImage}
         className="w-fit h-50 bg-slate-700 px-2 text-white"
       >
         snapshot
       </button>
       <div className="w-1200 h-600 mt-20 border-2">
         <Suspense fallback={"loading"}>
-          <Canvas
-            ref={canvasRef}
-            gl={{ preserveDrawingBuffer: true }}
-            camera={{ position: [0, 0, 10] }}
-          >
+          <Canvas ref={canvasRef} gl={{ preserveDrawingBuffer: true }}>
             <Environment preset="sunset" />
             <Stage>
-              <Viewer />
+              {/* <Viewer /> */}
+              <Viewer3D
+                capturing={capturing}
+                setCapturing={setCapturing}
+                capture={capture}
+              />
             </Stage>
             <OrbitControls ref={controlsRef} />
           </Canvas>
--- components/Viewer3D.tsx
+++ components/Viewer3D.tsx
@@ -1,17 +1,53 @@
 "use client";
 
-import { forwardRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { useThree, Canvas } from "@react-three/fiber";
+import { useThree, useFrame } from "@react-three/fiber";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
-const Viewer3D = () => {
+const Viewer3D = ({
+  capturing,
+  setCapturing,
+  capture,
+}: {
+  capturing: boolean;
+  setCapturing: (capture: boolean) => void;
+  capture: () => void;
+}) => {
   const path = "/models/deer.glb";
 
   const result = useGLTF(path);
   result.scene.updateMatrixWorld();
 
-  return <primitive object={result.scene} />;
+  const modelRef = useRef<any>();
+  // const [capturing, setCapturing] = useState<boolean>(false);
+  const [currentAngle, setCurrentAngle] = useState<number>(0);
+
+  // const captureImage = () => {
+  //   if (!capturing) {
+  //     setCapturing(true);
+  //   }
+  // };
+
+  useFrame(() => {
+    if (capturing) {
+      modelRef.current.rotation.y += (Math.PI / 180) * 30;
+      setCurrentAngle(prevState => prevState + 30);
+    }
+  });
+
+  useEffect(() => {
+    if (capturing) {
+      capture();
+
+      if (currentAngle >= 360) {
+        setCapturing(false);
+        setCurrentAngle(0);
+      }
+    }
+  }, [capturing, currentAngle]);
+
+  return <primitive object={result.scene} ref={modelRef} />;
 };
 
-export default forwardRef(Viewer3D);
+export default Viewer3D;
